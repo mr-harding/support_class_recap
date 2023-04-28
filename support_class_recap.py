91a55a616afe18b75a295ddad5eed0faa5cb5cd4
mr-harding <45374233+mr-harding@users.noreply.github.com>
2023-04-28 15:20:35 +1200

    GUI class added with label, button and method to test

--- support_class_recap.py
+++ support_class_recap.py
@@ -6,6 +6,24 @@ class Student:
 		self.name = name
 		self.credits = credits
 
+class StudentGUI:
+	def __init__(self, parent):
+		label1 = Label(parent, text = "STUDENT NCEA DATA MANAGER THING")
+		label1.pack()
+
+		go_btn = Button(parent, text = "Who has passed NCEA?", command = self.check_pass)
+		go_btn.pack()
+
+	def check_pass(self):
+		CREDITS_REQUIRED = 80
+		print("No data yet..")		
+
+if __name__ == "__main__":
+	root = Tk()
+	w = StudentGUI(root)
+	root.mainloop()
+
+"""
 students = []
 students.append(Student("Jane", 85))
 students.append(Student("Joan", 65))
@@ -13,3 +31,4 @@ students.append(Student("Jess", 80))
 
 for s in students:
 	print(s.name, "has", s.credits, "credits")
+"""
