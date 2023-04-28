88b9d1144c96e478654e0e36e340e82ac2c2c60d
mr-harding <45374233+mr-harding@users.noreply.github.com>
2023-04-28 15:30:00 +1200

    Could also run methods from within the support class

--- support_class_recap.py
+++ support_class_recap.py
@@ -2,30 +2,40 @@ from tkinter import *
 from tkinter import messagebox
 
 class Student:
-	def __init__(self, name, credits):
-		self.name = name
-		self.credits = credits
+    def __init__(self, name, credits):
+        self.name = name
+        self.credits = credits
+
+    def display_pass(self):
+        messagebox.showinfo("PASSED!", "{} has passed with {} credits".format(self.name, self.credits))
+    
+    def display_fail(self):
+        messagebox.showerror(":(", "{} has not yet passed. {} more credits needed".format(self.name, 80 - self.credits))	
 
 class StudentGUI:
-	def __init__(self, parent):
-		self.students = []
-		self.students.append(Student("Jane", 85))
-		self.students.append(Student("Joan", 65))
-		self.students.append(Student("Jess", 80))
+    def __init__(self, parent):
+        self.students = []
+        self.students.append(Student("Jane", 85))
+        self.students.append(Student("Joan", 65))
+        self.students.append(Student("Jess", 80))
 
-		label1 = Label(parent, text = "STUDENT NCEA DATA MANAGER THING")
-		label1.pack()
+        label1 = Label(parent, text = "STUDENT NCEA DATA MANAGER THING")
+        label1.pack()
 
-		go_btn = Button(parent, text = "Who has passed NCEA?", command = self.check_pass)
-		go_btn.pack()
+        go_btn = Button(parent, text = "Who has passed NCEA?", command = self.check_pass)
+        go_btn.pack()
+            
+    def check_pass(self):
+        CREDITS_REQUIRED = 80
+        for student in self.students:
+            if student.credits >= CREDITS_REQUIRED:
+                 student.display_pass()
+            else:
+                 student.display_fail()
+         
 
-	def check_pass(self):
-		CREDITS_REQUIRED = 80
-		for student in self.students:
-			if student.credits >= CREDITS_REQUIRED:
-				messagebox.showinfo("PASSED!", "{} has passed with {} credits".format(student.name, student.credits))
-			else:
-				messagebox.showerror(":(", "{} has not yet passed. {} more credits needed".format(student.name, CREDITS_REQUIRED - student.credits))	
+
+	
 
 if __name__ == "__main__":
 	root = Tk()
